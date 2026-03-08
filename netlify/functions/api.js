8fdc0cce9092d9c5a5adf72ae19d473753c623e6
Awesomedisha <awesomedisha@users.noreply.github.com>
2026-03-08 20:52:54 +0530

    Fix: Use esbuild bundler for Netlify functions to resolve ESM 502 crash

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,9 @@
 import serverless from 'serverless-http';
 import app from '../../server.js';
 
-// Wrap Express app for Netlify serverless functions
 export const handler = serverless(app);
+
+// Netlify function config - use esbuild for ESM support
+export const config = {
+    generator: '@netlify/functions-utils',
+};
